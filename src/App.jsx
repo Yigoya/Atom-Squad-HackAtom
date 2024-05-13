0ccddd06b91129663b875f97064f13f1d56ac8b2
Yohana <130049346+Yohanamtesfaye@users.noreply.github.com>
2024-05-13 14:06:04 +0300

    Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Landing } from './Pages/Landing'
 
 function App() {
   const [count, setCount] = useState(0)
   const position = [51.505, -0.09]
   return (
     <>
-      <h1>pusheshkut</h1>
+      <Landing/>
     </>
   )
 }
