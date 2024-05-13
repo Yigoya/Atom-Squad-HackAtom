1ceff3f73c2357e9fb2775be51410a3857b5fcda
yigoya <yigoya7@gmail.com>
2024-05-13 11:02:05 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const position = [51.505, -0.09]
+  return (
+    <>
+      <h1>pusheshkut</h1>
+    </>
+  )
+}
+
+export default App
